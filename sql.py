a6c379fb6e5a6768c3428471440d5f9777e88c5a
Artem Usachov <ar.usa4yov@yandex.ru>
2023-10-06 07:45:19 +0300

    one more fix

--- sql.py
+++ sql.py
@@ -1,25 +1,21 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("ProductsData").getOrCreate()
 
-products_data = []
+class ProductData:
+    def __init__(self, products_data=(), categories_data=(), categories_of_products_data=()):
+        self.spark = SparkSession.builder.appName("ProductsData").getOrCreate()
 
-categories_data = []
+        products = self.spark.createDataFrame(products_data)
+        categories = self.spark.createDataFrame(categories_data)
+        categories_of_products = self.spark.createDataFrame(categories_of_products_data)
 
-categories_of_products_data = []
+        products.createOrReplaceTempView("products")
+        categories.createOrReplaceTempView("categories")
+        categories_of_products.createOrReplaceTempView("product_categories")
 
+    def get_categories_of_products(self):
+        results = self.spark.sql("SELECT product_name, category_name FROM products \
+                            LEFT JOIN product_categories ON products.product_id = product_categories.product_id \
+                            LEFT JOIN categories c ON product_categories.category_id = categories.category_id")
 
-products = spark.createDataFrame(products_data)
-categories = spark.createDataFrame(categories_data)
-categories_of_products = spark.createDataFrame(categories_of_products_data)
-
-
-products.createOrReplaceTempView("products")
-categories.createOrReplaceTempView("categories")
-categories_of_products.createOrReplaceTempView("product_categories")
-
-results = spark.sql("SELECT product_name, category_name FROM products \
-                    LEFT JOIN product_categories ON products.product_id = product_categories.product_id \
-                    LEFT JOIN categories c ON product_categories.category_id = categories.category_id")
-
-results.show()
+        results.show()
